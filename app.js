d2a97228c75ab576cd4ec771eab9cee14a155f50
GRACE KEZA <kezagrace17@gmail.com>
2019-10-24 20:03:36 +0200

    [Fixes #169264762] Adding headers to appjs and and bodyparser

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.use(bodyParser.json());
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allowed-Methods', 'GET', 'POST', 'PUT', 'DELETE');
+  res.setHeader('Access-Control-Allowed-Headers', 'Content-Type', 'Authorisation');
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
 app.listen(3000);
